d69ccc814085f349d95dcf96993f673a0ffbbb0a
danher2 <daniel.hernandez8020@gmail.com>
2021-06-04 16:58:42 -0600

    video 38

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +26,10 @@ import com.netflix.ribbon.proxy.annotation.Hystrix;
 public class ItemController {
 	
 	private static Logger log = LoggerFactory.getLogger(ItemController.class);
+	
+	@Autowired
+	private Environment env;
+	
 	
 	@Autowired
 	@Qualifier("serviceFeign")
@@ -64,6 +69,12 @@ public class ItemController {
 		Map<String, String> json = new HashMap<String, String>();
 		json.put("texto", texto);
 		json.put("puerto", puerto);
+		
+		if (env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+
+		}
 		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
 }
