eb3df452c94de88b79fca6cbf0315e173f6e22e4
danher2 <daniel.hernandez8020@gmail.com>
2021-06-04 15:48:20 -0600

    36. Leer configuracion con Anotacion value

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -1,9 +1,16 @@
 package com.formacionbdi.springboot.app.item.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +24,13 @@ import com.netflix.ribbon.proxy.annotation.Hystrix;
 @RestController
 public class ItemController {
 	
+	private static Logger log = LoggerFactory.getLogger(ItemController.class);
+	
 	@Autowired
 	@Qualifier("serviceFeign")
 	private ItemService itemService;
+	@Value("${configuracion.texto}")
+	private String texto;
 	
 	@GetMapping("/listar")
 	public List<Item> listar(){
@@ -47,4 +58,12 @@ public class ItemController {
 		
 	}
 
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto){
+		log.info(texto);
+		Map<String, String> json = new HashMap<String, String>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+	}
 }
