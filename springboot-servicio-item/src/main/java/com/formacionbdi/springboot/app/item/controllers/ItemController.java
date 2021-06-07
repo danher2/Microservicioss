19fb7b2f8a9192fba7a8c8bcee8b861ba12600e0
danher2 <daniel.hernandez8020@gmail.com>
2021-06-07 14:42:55 -0600

    video 46

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -13,8 +13,13 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.springboot.app.item.models.Item;
@@ -35,7 +40,7 @@ public class ItemController {
 	
 	
 	@Autowired
-	@Qualifier("serviceFeign")
+	@Qualifier("serviceRestTemplate")
 	private ItemService itemService;
 	
 	@Value("${configuracion.texto}") //to pass properties information
@@ -82,4 +87,28 @@ public class ItemController {
 		}
 		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
 	}
+	
+	
+	//Endpoint para llamar los metodos del controlador de Producto mediante RestClient
+	@PostMapping("/crear")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto crear (@RequestBody Producto producto) {
+		return itemService.save(producto);
+	}
+	
+	@PutMapping("/editar/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto editar (@RequestBody Producto producto, @PathVariable Long id) {
+		return itemService.update(producto, id);
+		
+	}
+	
+	
+	@DeleteMapping("/eliminar/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void eliminar(@PathVariable Long id){
+		itemService.delete(id);
+				
+	}
+		
 }
