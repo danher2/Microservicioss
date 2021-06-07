ef076e847f1d091db60557e85afc6aac724d01d5
danher2 <daniel.hernandez8020@gmail.com>
2021-06-07 11:14:29 -0600

    hasta el video 44

--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/controllers/ProductoController.java
@@ -5,9 +5,15 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 //import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formacionbdi.springboot.app.productos.models.entity.Producto;
@@ -48,6 +54,31 @@ public class ProductoController {
 		}*/
 			
 		return producto;
+	}
+	
+	@PostMapping("/crear")
+	@ResponseStatus(HttpStatus.CREATED) //codigo 201 
+	public Producto crear(@RequestBody Producto producto) {
+		return productoService.save(producto);
+	}
+	
+	
+	@PutMapping("/editar/{id}")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Producto editar(@RequestBody Producto producto, @PathVariable Long id) {
+		Producto productodb = productoService.findById(id);
+		productodb.setNombre(producto.getNombre());
+		productodb.setPrecio(producto.getPrecio());
+		
+		return productoService.save(productodb);
+	}
+	
+	
+	@DeleteMapping("/eliminar/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void eliminar(@PathVariable Long id){
+		productoService.deleteById(id);
+				
 	}
 
 }
