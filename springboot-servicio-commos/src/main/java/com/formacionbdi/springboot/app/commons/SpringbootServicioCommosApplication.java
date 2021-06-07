c022c4d17a53dbd7afe74342def64efe4d0f6234
danher2 <daniel.hernandez8020@gmail.com>
2021-06-07 16:51:54 -0600

    up to 50 video

--- springboot-servicio-commos/src/main/java/com/formacionbdi/springboot/app/commons/SpringbootServicioCommosApplication.java
+++ springboot-servicio-commos/src/main/java/com/formacionbdi/springboot/app/commons/SpringbootServicioCommosApplication.java
@@ -1,9 +1,12 @@
 package com.formacionbdi.springboot.app.commons;
 
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})//para deshabilitar la autoconfiguracion de la satasource porque quite el la dependencia h2
 public class SpringbootServicioCommosApplication {
 
 	
--- springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/SpringbootServicioProductosApplication.java
@@ -2,10 +2,12 @@ package com.formacionbdi.springboot.app.productos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EntityScan({"com.formacionbdi.springboot.app.commons.models.entity"})//como las clases entity la tengo en otro proyecto con esto le digo que la busque en el otro proyecto
 public class SpringbootServicioProductosApplication {
 
 	public static void main(String[] args) {
