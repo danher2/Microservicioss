870d8cb9beece419e5f6309f164a91c1ca6e62bd
danher2 <daniel.hernandez8020@gmail.com>
2021-06-10 18:23:57 -0600

    81 funcionando

--- springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/services/UsuarioService.java
+++ springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/services/UsuarioService.java
@@ -3,7 +3,6 @@ package com.formacionbdi.springboot.app.oauth.services;
 import java.util.List;
 import java.util.stream.Collectors;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,50 +17,50 @@ import org.springframework.stereotype.Service;
 import com.formacionbdi.springboot.app.commons.usuarios.models.entity.Usuario;
 import com.formacionbdi.springboot.app.oauth.clients.UsuarioFeignClient;
 
+import feign.FeignException;
 
-@Service //registrar un bean o un componente en el contenedor de Spring
+@Service // registrar un bean o un componente en el contenedor de Spring
 public class UsuarioService implements IUsuarioService, UserDetailsService {
 
-
 	private Logger log = LoggerFactory.getLogger(UsuarioService.class);
-	
-	
+
 	@Autowired
 	private UsuarioFeignClient client;
-	
-	//obtiene al usuario por el username
+
+	// obtiene al usuario por el username
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
-		Usuario usuario =client.findByUsername(username);
-		
-		
-		
-		if (usuario == null) {
+
+		try {
+
+			Usuario usuario = client.findByUsername(username);
+
+			// convierte una lista de tipo rol a una lista de tipo GrantedAuthority
+			List<GrantedAuthority> authorities = usuario.getRoles().stream()
+					.map(role -> new SimpleGrantedAuthority(role.getNombre()))
+					.peek(authority -> log.info("Role: " + authority.getAuthority())).collect(Collectors.toList());
+			log.info("Usuario autenticado: " + username);
+
+			return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(), true, true, true,
+					authorities);
+
+		} catch (FeignException e) {
 			log.error("Error en el login no existe el usuario " + username + " en el sistema");
-			throw new UsernameNotFoundException("Error en el login no existe el usuario " + username + " en el sistema");
+			throw new UsernameNotFoundException(
+					"Error en el login no existe el usuario " + username + " en el sistema");
+
 		}
-											//convierte una lista de tipo rol a una lista de tipo GrantedAuthority
-		List<GrantedAuthority> authorities = usuario.getRoles().stream()
-											.map(role -> new SimpleGrantedAuthority(role.getNombre()))
-											.peek(authority -> log.info("Role: "+ authority.getAuthority()))
-											.collect(Collectors.toList());
-										log.info("Usuario autenticado: " + username);
-		
-		
-		return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(),
-						true, true, true, authorities);
 	}
 
 	@Override
 	public Usuario findByUsername(String username) {
-		
+
 		return client.findByUsername(username);
 	}
 
 	@Override
 	public Usuario update(Usuario usuario, Long id) {
-			
+
 		return client.update(usuario, id);
 	}
 
