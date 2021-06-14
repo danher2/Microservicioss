436bc8fa0b356a4f37c67db537ca2b719fdbbc76
danher2 <daniel.hernandez8020@gmail.com>
2021-06-14 16:28:31 -0600

    video 99 funcionando

--- springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/security/event/AuthenticationSuccesErrorHandler.java
+++ springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/security/event/AuthenticationSuccesErrorHandler.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Component;
 import com.formacionbdi.springboot.app.commons.usuarios.models.entity.Usuario;
 import com.formacionbdi.springboot.app.oauth.services.IUsuarioService;
 
+import brave.Tracer;
 import feign.FeignException;
 
 @Component
@@ -19,6 +20,9 @@ public class AuthenticationSuccesErrorHandler implements AuthenticationEventPubl
 
 	@Autowired
 	private IUsuarioService usuarioService;
+	
+	@Autowired
+	private Tracer tracer;
 
 	private Logger log = LoggerFactory.getLogger(AuthenticationSuccesErrorHandler.class);
 
@@ -53,24 +57,34 @@ public class AuthenticationSuccesErrorHandler implements AuthenticationEventPubl
 		System.out.println(mensaje);
 
 		try {
-
+			
+			StringBuilder errors = new StringBuilder();
+			errors.append(mensaje);
+			
 			Usuario usuario = usuarioService.findByUsername(authentication.getName());
 			if (usuario.getIntentos() == null) {
 				usuario.setIntentos(0);
 			}
 
 			log.info("Intentos actuales  de: " + usuario.getIntentos());
+			
 			usuario.setIntentos(usuario.getIntentos() + 1);
+			
 			log.info("Intentos despues  de: " + usuario.getIntentos());
-
+			
+			errors.append("  - Intentos del login: " + usuario.getIntentos());
+			
 			if (usuario.getIntentos() >= 3) {
-
-				log.error(String.format("El usuario %s des-habilitado por 3 intentos ", usuario.getUsername()));
+				
+				String errorMaxIntentos = String.format("El usuario %s des-habilitado por 3 intentos ", usuario.getUsername());
+				log.error(errorMaxIntentos);
+				errors.append(" - " + errorMaxIntentos);
 				usuario.setEnabled(false);
 			}
 
 			usuarioService.update(usuario, usuario.getId());
-
+			tracer.currentSpan().tag("error.mensaje", errors.toString());
+			
 		} catch (FeignException e) {
 			log.error(String.format("El usuario %s no existe en el sistema", authentication.getName()));
 
--- springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/services/UsuarioService.java
+++ springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/services/UsuarioService.java
@@ -17,6 +17,7 @@ import org.springframework.stereotype.Service;
 import com.formacionbdi.springboot.app.commons.usuarios.models.entity.Usuario;
 import com.formacionbdi.springboot.app.oauth.clients.UsuarioFeignClient;
 
+import brave.Tracer;
 import feign.FeignException;
 
 @Service // registrar un bean o un componente en el contenedor de Spring
@@ -26,6 +27,9 @@ public class UsuarioService implements IUsuarioService, UserDetailsService {
 
 	@Autowired
 	private UsuarioFeignClient client;
+	
+	@Autowired
+	private Tracer tracer;
 
 	// obtiene al usuario por el username
 	@Override
@@ -45,7 +49,12 @@ public class UsuarioService implements IUsuarioService, UserDetailsService {
 					authorities);
 
 		} catch (FeignException e) {
-			log.error("Error en el login no existe el usuario " + username + " en el sistema");
+			
+			String error = "Error en el login no existe el usuario " + username + " en el sistema";
+			log.error(error);
+			tracer.currentSpan().tag("error.mensaje", error + ": " + e.getMessage());
+			
+			
 			throw new UsernameNotFoundException(
 					"Error en el login no existe el usuario " + username + " en el sistema");
 
